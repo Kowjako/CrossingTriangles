04fc5982d3a85d80bde3bacb61e9e8f75ce86707
Vladimir <kowyako@yandex.ru>
2021-01-03 16:06:27 +0200

    Add project files.

---
+++ ZhenyaLab/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace ZhenyaLab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.y3 = new System.Windows.Forms.TextBox();
+            this.x3 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.y2 = new System.Windows.Forms.TextBox();
+            this.x2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.y1 = new System.Windows.Forms.TextBox();
+            this.x1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(602, 392);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.y3);
+            this.groupBox1.Controls.Add(this.x3);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.y2);
+            this.groupBox1.Controls.Add(this.x2);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.y1);
+            this.groupBox1.Controls.Add(this.x1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 410);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(209, 137);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nowy trójkąt";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 29);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Utwórz trójkąt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // y3
+            // 
+            this.y3.Location = new System.Drawing.Point(168, 79);
+            this.y3.Name = "y3";
+            this.y3.Size = new System.Drawing.Size(30, 20);
+            this.y3.TabIndex = 14;
+            // 
+            // x3
+            // 
+            this.x3.Location = new System.Drawing.Point(120, 79);
+            this.x3.Name = "x3";
+            this.x3.Size = new System.Drawing.Size(30, 20);
+            this.x3.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(153, 79);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 20);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "У";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(105, 79);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(17, 20);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Х";
+            // 
+            // y2
+            // 
+            this.y2.Location = new System.Drawing.Point(168, 53);
+            this.y2.Name = "y2";
+            this.y2.Size = new System.Drawing.Size(30, 20);
+            this.y2.TabIndex = 10;
+            // 
+            // x2
+            // 
+            this.x2.Location = new System.Drawing.Point(120, 53);
+            this.x2.Name = "x2";
+            this.x2.Size = new System.Drawing.Size(30, 20);
+            this.x2.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(153, 53);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "У";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(105, 53);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 20);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Х";
+            // 
+            // y1
+            // 
+            this.y1.Location = new System.Drawing.Point(168, 27);
+            this.y1.Name = "y1";
+            this.y1.Size = new System.Drawing.Size(30, 20);
+            this.y1.TabIndex = 6;
+            // 
+            // x1
+            // 
+            this.x1.Location = new System.Drawing.Point(120, 27);
+            this.x1.Name = "x1";
+            this.x1.Size = new System.Drawing.Size(30, 20);
+            this.x1.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(153, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "У";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(105, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Х";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(6, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Wierzchołek C";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(6, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Wierzchołek B";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(6, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Wierzchołek А";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Location = new System.Drawing.Point(227, 410);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(387, 99);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Opcje";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(224, 63);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(157, 24);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Sprawdź";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(6, 63);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(163, 20);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Który ma największe pole";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(224, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 24);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Sprawdź";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(6, 27);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(185, 20);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "Czy przecinają się prostokąty";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OliveDrab;
+            this.ClientSize = new System.Drawing.Size(626, 559);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Opacity = 0.95D;
+            this.Text = "Triangles";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox y3;
+        private System.Windows.Forms.TextBox x3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox y2;
+        private System.Windows.Forms.TextBox x2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox y1;
+        private System.Windows.Forms.TextBox x1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ ZhenyaLab/Form1.cs
@@ -0,0 +1,79 @@
+﻿using Library10;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZhenyaLab
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        List<Triangle> t;
+        public Form1()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+            g = panel1.CreateGraphics();
+            t = new List<Triangle>(2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (t.Count < 2)
+            {
+                t.Add(Functionality.newTriangle(Int32.Parse(x1.Text) * 2, Int32.Parse(y1.Text) * 2, Int32.Parse(x2.Text) * 2, Int32.Parse(y2.Text) * 2, Int32.Parse(x3.Text) * 2, Int32.Parse(y3.Text) * 2));
+            }
+            else MessageBox.Show("Два треугольника уже созданы !", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            panel1.Refresh();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (t.Count != 0)
+            {
+                drawTriangle(e.Graphics);
+            }
+        } 
+        public void drawTriangle(Graphics g)
+        {
+            Color[] colors = { Color.Red, Color.Blue };
+            int i = 0;
+            foreach (Triangle t in t)
+            {
+                Pen myPen = new Pen(colors[i]);
+                myPen.Width = 2.0F;
+                g.DrawLine(myPen, t.vertex1.x, t.vertex1.y, t.vertex2.x, t.vertex2.y);
+                g.DrawLine(myPen, t.vertex1.x, t.vertex1.y, t.vertex3.x, t.vertex3.y);
+                g.DrawLine(myPen, t.vertex2.x, t.vertex2.y, t.vertex3.x, t.vertex3.y);
+                myPen.Dispose();
+                i++;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (t.Count == 2)
+            {
+                if (Functionality.comapreTriangles(t[0], t[1]) == 1) MessageBox.Show("Первый больше", "Сравнение площадей", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else MessageBox.Show("Второй больше", "Сравнение площадей", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else MessageBox.Show("Создайте два треугольника!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (t.Count == 2)
+            {
+                if (Functionality.checkIfCoveredByAnother(t[0],t[1]) == true) MessageBox.Show("Треугольники пересекаются!", "Проверка пересечения", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else MessageBox.Show("Нет пересечения!", "Проверка пересечения", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else MessageBox.Show("Создайте два треугольника!", "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
